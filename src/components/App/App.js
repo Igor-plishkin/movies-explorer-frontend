19651f306e37ae86821b7361a14d9949ed629640
Igor-plishkin <Plisha.jr@yandex.ru>
2021-11-09 22:02:15 +0500

    add header

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,21 +1,10 @@
 import './App.css';
+import Header from '../Header/Header';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Регистрация
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
     </div>
   );
 }
