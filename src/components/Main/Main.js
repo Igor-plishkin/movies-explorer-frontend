621076963da61de2732cf77d703a4148c32f3d05
Igor-plishkin <Plisha.jr@yandex.ru>
2021-11-10 22:45:44 +0500

    section techs

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 function Main() {
   return (
     <>
-      <section className="promo">
+      <section className="promo section">
         <div className="promo__container">
           <h1 className="promo__title">
             Учебный проект студента факультета Веб-разработки.
@@ -16,8 +16,8 @@ function Main() {
           </nav>
         </div>
       </section>
-      <section className="about">
-        <h2 className="about__title">О проекте</h2>
+      <section className="about section">
+        <h2 className="section-title section-title_about">О проекте</h2>
         <hr className="horizontal-line"/>
         <div className="about__content">
           <p className="about__main-text">Дипломный проект включал 5 этапов</p>
@@ -30,6 +30,23 @@ function Main() {
           <div className="about__time about__time_four-weeks">4 недели</div>
           <span className="about__sub-time">Back-end</span>
           <span className="about__sub-time">Front-end</span>
+        </div>
+      </section>
+      <section className="techs section">
+        <h2 className="section-title section-title_techs">Технологии</h2>
+        <hr className="horizontal-line"/>
+        <div className="techs__content">
+          <p className="techs__title-text">7 технологий</p>
+          <p className="techs__text">На курсе веб-разработки мы освоили технологии, которые применили в дипломном проекте.</p>
+          <ul className="techs__list">
+            <li className="techs__list-item">HTML</li>
+            <li className="techs__list-item">CSS</li>
+            <li className="techs__list-item">JS</li>
+            <li className="techs__list-item">React</li>
+            <li className="techs__list-item">Git</li>
+            <li className="techs__list-item">Express.js</li>
+            <li className="techs__list-item">mongoDB</li>
+          </ul>
         </div>
       </section>
     </>
