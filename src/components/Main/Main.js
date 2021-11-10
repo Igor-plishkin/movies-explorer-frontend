4d5ecd78a093b9cf7850906d856d8d4e3a774a3f
Igor-plishkin <Plisha.jr@yandex.ru>
2021-11-10 22:25:25 +0500

    section about

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -17,7 +17,20 @@ function Main() {
         </div>
       </section>
       <section className="about">
-
+        <h2 className="about__title">О проекте</h2>
+        <hr className="horizontal-line"/>
+        <div className="about__content">
+          <p className="about__main-text">Дипломный проект включал 5 этапов</p>
+          <p className="about__main-text">На выполнение диплома ушло 5 недель</p>
+          <p className="about__sub-text">Составление плана, работу над бэкендом, вёрстку, добавление функциональности и финальные доработки.</p>
+          <p className="about__sub-text">У каждого этапа был мягкий и жёсткий дедлайн, которые нужно было соблюдать, чтобы успешно защититься.</p>
+        </div>
+        <div className="about__timeline">
+          <div className="about__time about__time_one-week">1 неделя</div>
+          <div className="about__time about__time_four-weeks">4 недели</div>
+          <span className="about__sub-time">Back-end</span>
+          <span className="about__sub-time">Front-end</span>
+        </div>
       </section>
     </>
   );
