8b98c7ca789fb81b0056910f1115162efe4a78e4
Igor-plishkin <Plisha.jr@yandex.ru>
2021-11-17 00:15:40 +0500

    Burger Menu

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -12,15 +12,15 @@ import NotFound from "../NotFound/NotFound";
 function App() {
   return (
     <div className="App">
-      {/* <Header /> */}
+      <Header />
       {/* <Main /> */}
-      {/* <Movies /> */}
+      <Movies />
       {/* <SavedMovies /> */}
       {/* <Profile /> */}
       {/* <Register /> */}
       {/* <Login /> */}
-      <NotFound/>
-      {/* <Footer /> */}
+      {/* <NotFound/> */}
+      <Footer />
     </div>
   );
 }
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,13 +2,27 @@ import { Link } from "react-router-dom";
 import logo from "../../images/logo.svg";
 import accountIcon from "../../images/account-icon.svg";
 import "./Header.css";
+import React, { useState } from "react";
+import BurgerMenu from "../BurgerMenu/BurgerMenu";
 
 function Header() {
+  const [isOpenMenu, setOpenMenu] = useState(false);
+
+    function handleOpenMenu(){
+    setOpenMenu(true);
+  }
+  function handleCloseMenu(){
+    setOpenMenu(false);
+  }
+  // заглушка
+  const isLogged = true;
+  const mobile = window.matchMedia("(max-width: 1023px)").matches;
+
   return (
     <header className="header">
       <img src={logo} alt="Логотип" className="header__logo" />
 
-      {true ? (
+      {!isLogged ? (
         <div className="header__login">
           <Link className="header__link header__link_singin" to="/sign-in">
             Регистрация
@@ -19,20 +33,36 @@ function Header() {
         </div>
       ) : (
         <>
-          <nav className="header__navigation">
-            <Link className="header__link header__link_films" to="/movies">Фильмы</Link>
-            <Link className="header__link header__link_saved-films" to="/saved-movies">Сохранённые фильмы</Link>
-          </nav>
-          <Link className="header__link header__link_account" to="/profile">
-            Аккаунт
-            <img
-              className="header__account-icon"
-              src={accountIcon}
-              alt="аккаунт"
-            />
-          </Link>
+          {mobile ? (
+            <>
+              <button className={`header__burger-btn ${isOpenMenu && "header__burger-btn_active"}`} onClick={handleOpenMenu}></button>
+            </>
+          ) : (
+            <>
+              <nav className="header__navigation">
+                <Link className="header__link header__link_films" to="/movies">
+                  Фильмы
+                </Link>
+                <Link
+                  className="header__link header__link_saved-films"
+                  to="/saved-movies"
+                >
+                  Сохранённые фильмы
+                </Link>
+              </nav>
+              <Link className="header__link header__link_account" to="/profile">
+                Аккаунт
+                <img
+                  className="header__account-icon"
+                  src={accountIcon}
+                  alt="аккаунт"
+                />
+              </Link>
+            </>
+          )}
         </>
       )}
+      <BurgerMenu isOpen={isOpenMenu} onClose={handleCloseMenu}/>
     </header>
   );
 }
