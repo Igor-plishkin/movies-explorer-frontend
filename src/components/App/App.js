ab65e3715b1561c07230e03a8a636245e9021129
Igor-plishkin <Plisha.jr@yandex.ru>
2021-11-22 20:45:15 +0500

    part of auth

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -8,14 +8,74 @@ import Profile from "../Profile/Profile";
 import Register from "../Register/Register";
 import Login from "../Login/Login";
 import NotFound from "../NotFound/NotFound";
-import { Route, Routes } from "react-router-dom";
-import getMovies from "../../utils/MainApi";
+import { Route, Routes, useNavigate } from "react-router-dom";
+import getMovies from "../../utils/MoviesApi";
 import React from "react";
+import auth from "../../utils/auth";
+import { CurrentUserContext } from "../../contexts/CurrentUserContext";
 
 function App() {
+  const navigate = useNavigate();
+
   const [movies, setMovies] = React.useState([]);
 
+  const [loggedIn, setLoggedIn] = React.useState(false);
+  const [currentUser, setCurrentUser] = React.useState({});
+
   React.useEffect(() => {
+    const jwt = localStorage.getItem("jwt");
+
+    if (jwt) {
+      auth
+        .getToken(jwt)
+        .then((res) => {
+          setLoggedIn(true);
+          navigate("/movies");
+        })
+        .catch((err) => {
+          if (err.status === 400) {
+            console.log("400 — Токен не передан или передан не в том формате");
+          } else if (err.status === 401) {
+            console.log("401 — Переданный токен некорректен");
+          }
+        });
+    }
+  }, [navigate]);
+
+  function handleLogin(email, password) {
+    auth
+      .login(email, password)
+      .then((res) => {
+        localStorage.setItem("jwt", res.token);
+        setLoggedIn(true);
+        navigate("/movies");
+      })
+      .catch((err) => {
+        if (err.status === 400) {
+          console.log("400 - не передано одно из полей");
+        } else if (err.status === 401) {
+          console.log("401 - пользователь с email не найден");
+        }
+      });
+  }
+
+  function handleRegistr(email, password) {
+    auth
+      .registration(email, password)
+      .then((res) => {
+        navigate("/signin");
+      })
+      .catch((err) => {
+        if (err.status === 400) {
+          console.log("400 - некорректно заполнено одно из полей");
+        }
+      });
+  }
+
+  React.useEffect(() => {
+    if (loggedIn){
+      Promise.all([])
+    }
     getMovies().then((res) => {
       console.log(res);
       setMovies(res);
@@ -52,8 +112,11 @@ function App() {
           }
         />
         <Route path="/profile" element={<Profile />} />
-        <Route path="/signup" element={<Register />} />
-        <Route path="/signin" element={<Login />} />
+        <Route
+          path="/signup"
+          element={<Register onRegistr={handleRegistr} />}
+        />
+        <Route path="/signin" element={<Login onLogin={handleLogin} />} />
         <Route path="/*" element={<NotFound />} />
       </Routes>
     </div>
--- src/components/MoviesCardList/MoviesCardList.js
+++ src/components/MoviesCardList/MoviesCardList.js
@@ -1,16 +1,21 @@
 import React from "react";
 import "./MoviesCardList.css";
 import MoviesCard from "../MoviesCard/MoviesCard";
-import { desktopWidth, tabletWidth, mobileWidth, smallDesktopWidth } from "../../utils/constants";
+import {
+  desktopWidth,
+  tabletWidth,
+  mobileWidth,
+  smallDesktopWidth,
+} from "../../utils/constants";
 
 function MoviesCardList({ movies }) {
   const [renderMovies, setRenderMovies] = React.useState([]);
   const [windowSize, setWindowSize] = React.useState(window.innerWidth);
 
-  function handlerChangeWidth () {
+  function handlerChangeWidth() {
     return setTimeout(() => {
-      setWindowSize(window.innerWidth)
-    }, 500)
+      setWindowSize(window.innerWidth);
+    }, 500);
   }
 
   function moviesCount() {
@@ -26,12 +31,12 @@ function MoviesCardList({ movies }) {
 
   React.useEffect(() => {
     setRenderMovies(movies.slice(0, moviesCount().count));
-  // eslint-disable-next-line react-hooks/exhaustive-deps
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [movies, windowSize]);
 
   React.useEffect(() => {
-    window.addEventListener('resize', handlerChangeWidth);
-  })
+    window.addEventListener("resize", handlerChangeWidth);
+  });
 
   return (
     <>
@@ -44,9 +49,11 @@ function MoviesCardList({ movies }) {
           })
         )}
       </ul>
-      <button className="movies__add-btn" onClick={handleMoviesAdd}>
-        Ещё
-      </button>
+      {movies.length > renderMovies.length ? (
+        <button className="movies__add-btn" onClick={handleMoviesAdd}>
+          Ещё
+        </button>
+      ) : null}
     </>
   );
 }
