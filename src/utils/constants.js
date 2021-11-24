ebfb426a3449234d5c8e8c9a5f32659623e504a6
Igor-plishkin <Plisha.jr@yandex.ru>
2021-11-25 02:07:36 +0500

    fix base url

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,6 +1,5 @@
 export const MOVIES_API_URL = 'https://api.nomoreparties.co';
-// export const BASE_API_URL = 'https://api.plisha-movies.nomoredomains.icu';
-export const BASE_API_URL = 'http://localhost:3000';
+export const BASE_API_URL = 'https://api.plisha-movies.nomoredomains.icu';
 
 export const oneHour = 60;
 
